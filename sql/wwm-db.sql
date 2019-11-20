4b5a9933814988627ed328d737c62d2e84a624cc
liuyandong <554030404@qq.com>
2019-11-20 10:09:12 +0800

    错误修改！！

--- sql/wwm-db.sql
+++ sql/wwm-db.sql
@@ -162,3 +162,28 @@ CREATE TABLE device
     deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT = '设备基本信息';
+
+DROP TABLE IF EXISTS supplier;
+CREATE TABLE supplier
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    code VARCHAR(20) NOT NULL COMMENT '编号',
+    name VARCHAR(20) NOT NULL COMMENT '名称',
+    linkman VARCHAR(20) NOT NULL COMMENT '联系人',
+    telephone_number VARCHAR(20) NOT NULL COMMENT '联系电话',
+    fax VARCHAR(20) NOT NULL COMMENT '传真',
+    address VARCHAR(255) NOT NULL COMMENT '详细地址',
+    postcode VARCHAR(20) NOT NULL COMMENT '邮编',
+    email VARCHAR(50) NOT NULL COMMENT '电子邮箱',
+    tax_number VARCHAR(50) NOT NULL COMMENT '税号',
+    bank VARCHAR(50) NOT NULL COMMENT '开户行',
+    account_number VARCHAR(50) NOT NULL COMMENT '开户行账号',
+    remark VARCHAR(255) NOT NULL COMMENT '备注',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT = '供应商信息表';
