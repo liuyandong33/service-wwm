6890ddbe461fc1d2c243cb477faeed354667b84f
liuyandong <554030404@qq.com>
2019-11-25 10:32:42 +0800

    错误修改！！

--- src/main/java/build/dream/wwm/Application.java
+++ src/main/java/build/dream/wwm/Application.java
@@ -19,37 +19,4 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-    public static long first(int[] array) {
-        int length = array.length;
-        int maxSum = array[0];
-        for (int i = 0; i < length; i++) {
-            for (int j = i; j < length; j++) {
-                int currSum = 0;
-                for (int k = i; k <= j; k++) {
-                    currSum += array[k];
-                }
-
-                if (currSum > maxSum) {
-                    maxSum = currSum;
-                }
-            }
-        }
-        return maxSum;
-    }
-
-    public static long second(int[] array) {
-        int length = array.length;
-        int maxSum = array[0];
-        for (int i = 0; i < length; i++) {
-            int sum = 0;
-            for (int j = i; j < length; j++) {
-                sum += array[j];
-                if (sum > maxSum) {
-                    maxSum = sum;
-                }
-            }
-        }
-        return maxSum;
-    }
 }
