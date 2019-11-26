b460b28cb1a7c58045b294a04c8943ce2c06b210
liuyandong <554030404@qq.com>
2019-11-26 16:17:00 +0800

    错误修改！！

--- sql/wwm-db.sql
+++ sql/wwm-db.sql
@@ -164,6 +164,38 @@ CREATE TABLE device
     deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
 ) COMMENT = '设备基本信息';
 
+DROP TABLE IF EXISTS technical_data;
+CREATE TABLE technical_data
+(
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'ID',
+    water_works_id BIGINT NOT NULL COMMENT '水厂ID',
+    device_id BIGINT NOT NULL COMMENT '设备ID',
+    device_code VARCHAR(20) NOT NULL COMMENT '设备编号',
+    device_type VARCHAR(20) NOT NULL COMMENT '设备类型',
+    flow DECIMAL(11, 3) NOT NULL COMMENT '流量（m³/h）',
+    power DECIMAL(11, 3) NOT NULL COMMENT '配用功率（KW）',
+    lift DECIMAL(11, 3) NOT NULL COMMENT '扬程（M）',
+    rotary_speed DECIMAL(11, 3) NOT NULL COMMENT '同步转速（r/min）',
+    caliber DECIMAL(11, 3) NOT NULL COMMENT '排出口径（mm）',
+    weight DECIMAL(11, 3) NOT NULL COMMENT '重量（KG）',
+    rated_voltage DECIMAL(11, 3) NOT NULL COMMENT '额定电压（V）',
+    rated_current DECIMAL(11, 3) NOT NULL COMMENT '额定电流（V）',
+    frequency DECIMAL(11, 3) NOT NULL COMMENT '频率（HZ）',
+    international_protection_marking VARCHAR(20) NOT NULL COMMENT '外壳防护等级',
+    insulation_level VARCHAR(20) NOT NULL COMMENT '绝缘等级',
+    phase VARCHAR(20) NOT NULL COMMENT '相数',
+    product_standard_no VARCHAR(50) NOT NULL COMMENT '产品标准编号',
+    production_license_no VARCHAR(50) NOT NULL COMMENT '生产许可证编号',
+    manufacturer VARCHAR(50) NOT NULL COMMENT '生产厂家',
+    created_time DATETIME NOT NULL DEFAULT NOW() COMMENT '创建时间',
+    created_user_id BIGINT NOT NULL COMMENT '创建人id',
+    updated_time DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW() COMMENT '最后更新时间',
+    updated_user_id BIGINT NOT NULL COMMENT '最后更新人id',
+    updated_remark VARCHAR(255) NOT NULL COMMENT '最后更新备注',
+    deleted_time DATETIME NOT NULL DEFAULT '1970-01-01 00:00:00' COMMENT '删除时间，只有当 deleted = 1 时有意义，默认值为1970-01-01 00:00:00',
+    deleted TINYINT NOT NULL DEFAULT 0 COMMENT '是否删除，0-未删除，1-已删除'
+) COMMENT = '主要技术参数';
+
 DROP TABLE IF EXISTS supplier;
 CREATE TABLE supplier
 (
